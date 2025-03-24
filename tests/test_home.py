472459ae06057de8b25418822183dcfb36effa6b
Alberto H. Cordeiro <acordeiro@3anet.com.br>
2025-03-23 22:40:48 -0300

    alteracoes do test_home.py

--- tests/test_home.py
+++ tests/test_home.py
@@ -4,5 +4,5 @@ def test_home(browser):
     url = "http://127.0.0.1:8081"
     browser.get(url)
     time.sleep(10)
-    assert browser.title == "Dashboard"
+    assert browser.title == "Dash"
     print("Teste da pagina inicial concluído com sucesso.")
